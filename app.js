e5dcbae894a579e591019cd7aa7bae1e5a1fca68
Shalitha-Lakshan <shalithalakshan046@gmail.com>
2026-02-23 14:16:22 +0530

    update

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ fs.writeFile('file.txt', 'Hello World!', function (err) {
   if (err) throw err;
   console.log('File saved!');
 });
+
+const fs = require('fs');
+
+fs.readFile('file.txt', 'utf8', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
